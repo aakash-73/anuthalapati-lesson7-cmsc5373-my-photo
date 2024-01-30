82f40f1b076f728186f1fe22b1a51682b3388c4d
Aakash <143157569+aakash-73@users.noreply.github.com>
2024-01-30 13:47:58 -0600

    firebase authentiaction added

--- public/controller/firebase_auth.js
+++ public/controller/firebase_auth.js
@@ -1,5 +1,18 @@
-export function signinFirebase(e){
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js"
+import { app } from "./firebase_core.js";
+
+const auth = getAuth(app);
+
+export async function signinFirebase(e){
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
+
+    try{
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+    }catch (error){
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    }
 }
