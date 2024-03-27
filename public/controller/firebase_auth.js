7b46485a899ca98e7c9f4f3c0cb69b2e97ed29e4
Aakash <143157569+aakash-73@users.noreply.github.com>
2024-03-27 18:07:03 -0500

    second commit

--- public/controller/firebase_auth.js
+++ public/controller/firebase_auth.js
@@ -1,4 +1,4 @@
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut,} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js"
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut, } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js"
 import { app } from "./firebase_core.js";
 import { DEV } from "../model/constants.js";
 import { homePageView, resetPhotoMemoList } from "../view/home_page.js";
@@ -10,50 +10,51 @@ const auth = getAuth(app);
 
 export let currentUser = null;
 
-export async function signinFirebase(e){
+export async function signinFirebase(e) {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
 
-    try{
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      //const user = userCredential.user;
-    }catch (error){
-        if(DEV) console.log('signin error: ', error);
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        //const user = userCredential.user;
+        
+    } catch (error) {
+        if (DEV) console.log('signin error: ', error);
         const errorCode = error.code;
         const errorMessage = error.message;
-        alert('Signin Error: '+ errorCode + ' '+ errorMessage);
+        alert('Signin Error: ' + errorCode + ' ' + errorMessage);
     }
 }
 
-export function attachAuthStateChangeObserver(){
+export function attachAuthStateChangeObserver() {
     onAuthStateChanged(auth, authStateChangeListener);
 }
 
-function authStateChangeListener(user){
+function authStateChangeListener(user) {
     currentUser = user;
-    if(user){
+    if (user) {
         userInfo.textContent = user.email;
         const postAuth = document.getElementsByClassName('myclass-postauth');
-        for (let i=0; i<postAuth.length;i++){
+        for (let i = 0; i < postAuth.length; i++) {
             postAuth[i].classList.replace('d-none', 'd-block');
         }
         const preAuth = document.getElementsByClassName('myclass-preauth');
-        for(let i=0; i<preAuth.length;i++){
+        for (let i = 0; i < preAuth.length; i++) {
             preAuth[i].classList('d-block', 'd-none');
         }
         const pathname = window.location.pathname;
         const hash = window.location.hash;
         routing(pathname, hash);
-    }else{
+    } else {
         userInfo.textContent = 'No User';
         resetPhotoMemoList();
         const postAuth = document.getElementsByClassName('myclass-postauth');
-        for (let i=0; i<postAuth.length;i++){
+        for (let i = 0; i < postAuth.length; i++) {
             postAuth[i].classList.replace('d-block', 'd-none');
         }
         const preAuth = document.getElementsByClassName('myclass-preauth');
-        for(let i=0; i<preAuth.length;i++){
+        for (let i = 0; i < preAuth.length; i++) {
             preAuth[i].classList('d-none', 'd-block');
         }
         history.pushState(null, null, routePathnames.HOME);
@@ -61,6 +62,6 @@ function authStateChangeListener(user){
     }
 }
 
-export async function signOutFirebase(){
+export async function signOutFirebase() {
     await signOut(auth);
 }
